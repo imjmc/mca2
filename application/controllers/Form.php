35ae4ffd40dd15901d3dd5951ebc344cd5ca4e2c
Jayanti <mail.to.jayanti@gmail.com>
2018-04-26 16:58:08 +0545

    latest

---
+++ application/controllers/Form.php
@@ -0,0 +1,49 @@
+<?php
+class Form extends CI_controller{
+	
+	function search(){
+		$age = (int)$this->input->post('age');
+		$term = (int)$this->input->post('term');
+		$inv = (int)$this->input->post('investment');
+        $ptype = $this->input->post('policy_type');
+
+		$this->load->model("Form_model");
+		$data['search'] = $this->Form_model->msearch($age, $term, $inv, $ptype);
+
+		$this->loadView('policy/result', $data,'policy/compareform');
+	}
+	public function loadView($page_name,$data,$compareform) {
+        $this->load->view('template/header');
+        /*
+        if($compareform){
+           
+            $this->load->view('policy/compareForm');
+            echo '</div>';
+        }
+        */
+        $this->load->view($page_name,$data);
+        $this->load->view('template/footer');
+    }
+    public function orderreceived(){
+    	if( $this->session->userdata('admin_id')){
+    	$id = $this->uri->segment('3');
+    	$order = array(
+            'policy_id' => $id,
+            'order_by' =>  $this->session->userdata('admin_id'),
+            'date' => date("Y/m/d")
+            
+        );
+        $this->load->model("Form_model");
+        $this->Form_model->add_order($order);
+
+    	$data['msg'] = "Received";
+    	$this->loadView('policy/thankYouPage',$data,'');
+    }
+    else
+    {
+    	$this->session->set_userdata('policy_id',$this->uri->segment('3'));
+    		redirect(base_url() . "admin");
+    }
+    }
+}
+?>
---
+++ application/controllers/Policy.php
@@ -0,0 +1,70 @@
+<?php
+
+class Policy extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->model('Policy_model');    
+    }
+
+    public function index() {
+        if (!$this->session->userdata('company_id')) {
+            redirect(base_url().'company', 'refresh');
+        }
+
+
+        $data = [];
+        $data['type_list'] = $this->Policy_model->get_all_types();
+        $this->loadView('add', $data);
+    }
+
+    public function add() {
+        $policy = array(
+            'name' => $this->input->post('name'),
+            'company_id' => $this->session->userdata('company_id'),
+            'policy_type_id' => $this->input->post('type_id'),
+            //'c_id' => $this->input->post('c_id'),
+            'inv_per_year' => $this->input->post('inv_per_year'),
+            'term' => $this->input->post('term'),
+            'expected_return' => $this->input->post('expected_return'),
+            'min_age' => $this->input->post('min_age'),
+            'max_age' => $this->input->post('max_age'),
+        );
+
+        $name_check = $this->Policy_model->name_check($policy['name']);
+        if ($name_check) {
+            $this->Policy_model->insert_policy($policy);
+            $this->session->set_flashdata('success_msg', 'Policy added successfully');
+            redirect(base_url().'policy/policy_list', 'refresh');
+        } else {
+            $this->session->set_flashdata('error_msg', 'Error occured,Try again.');
+            redirect(base_url().'policy', 'refresh');
+        }
+    }
+
+    public function policy_list(){
+        $data['company_id'] = $this->session->userdata('company_id');
+        if (!$this->session->userdata('company_id')) {
+            redirect(base_url().'company', 'refresh');
+        }
+
+        $data['policies'] = $this->Policy_model->list_company_policy($data['company_id']);
+        $this->loadView('list', $data);
+    }
+
+    public function editPolicy(){
+        $data = [];
+        $id = $this->uri->segment('3');
+        $data['get_policy'] = $this->Policy_model->get_policy($id);
+        $data['type_list'] = $this->Policy_model->get_all_types();
+        $this->loadView('add', $data);
+    }
+
+    public function loadView($page_name, $data) {
+        $this->load->view('template/header');
+        $this->load->view('policy/' . $page_name, $data);
+        $this->load->view('template/footer');
+    }
+}
+?>
