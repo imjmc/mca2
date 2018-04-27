e5ef5a97be98297de7a64293e093cf9c4a0007df
dell <sabitrakhl@gmail.com>
2018-04-27 18:44:35 +0545

    dashboard template added

---
+++ application/views/company/dashboard.php
@@ -0,0 +1,35 @@
+<p>&nbsp;</p>
+<div class="container-fluid ng-scope" ng-controller="WholeLifeController">
+    <div class="row">
+        <div class="info-block dark col-lg-3 col-md-2 hidden-xs hidden-sm p0" style="height: auto; position: static; margin-top: 1px; left: 0px; top: 95px;">
+            <div class="info-content">
+                <div class="form-area term-conditions">
+                    <ul class="nav in" id="side-menu">
+                            <li>
+                                <a href="<?PHP echo base_url();?>" class="active"><i class="fa fa-briefcase fa-fw"></i> Policies</a>
+                            </li>
+                            <li>
+                                <a href="<?PHP echo base_url();?>" class="active"><i class="fa fa-list fa-fw"></i> Orders</a>
+                            </li>
+                            <li>
+                                <a href="<?PHP echo base_url();?>" class="active"><i class="fa fa-user fa-fw"></i> Profile</a>
+                            </li>
+                            
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 content-list" style="min-height: 400px;">
+            <div class="form-area">
+                <form class="ng-pristine ng-valid">
+                    <div class="filter">
+                        <div class="pull-left com-btn">
+                            <a href="javascript:void(0);" class="link-btn-comp" ng-click="goBack()" tabindex="0"><i class="fa fa-arrow-left"></i> BACK</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
