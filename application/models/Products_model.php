77e95f73b19e4ae176a0cd1731c76d9c85110e4e
dell <sabitrakhl@gmail.com>
2018-04-27 22:06:27 +0545

    product page added

--- application/models/Products_model.php
+++ application/models/Products_model.php
@@ -6,8 +6,13 @@ class Products_model extends CI_model{
 	
 	public function listProducts(){
 		
-		$this->db->select('*');
-		$this->db->from('policies');
+		// $this->db->select('*');
+		// $this->db->from('policies');
+		// $this->db->join('companies', "companies.id = policies.company_id");
+		$this->db->select('policies.name AS policies_name,
+                          companies.name AS companies_name, policies.id, policies.inv_per_year, policies.term, policies.expected_return');
+        $this->db->from('policies');
+        $this->db->join('companies', 'companies.id = policies.company_id');
 		$query = $this->db->get();
         if ($query) {
             return $query->result();
--- application/views/products.php
+++ application/views/products.php
@@ -1,7 +1,69 @@
-<p>All Products List</p>
-<?php 
-foreach ($products as $product){ 
-?>
-	<a href="<?php echo base_url() ?>/<?php echo $product->id;?>"><?php echo $product->name; ?></a>
-<?php
-} ?>
+<p>&nbsp;</p>
+
+<p style="font-size: 30px;">All Available Policies</p>
+
+<div class="tab">
+    <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active" id="historical">
+            <div class="list-header">
+                <div class="list-head">
+                    <ul>
+                        <li>INSURER &amp; PLAN</li>
+                        <li>PREMIUM</li>
+                        <li>YEAR</li>
+                        <li>DETAILS</li>
+                        <li>&nbsp;</li>
+                    </ul>
+                </div>
+                <div class="list-offer ng-scope">
+                <?php
+                    foreach($products as $row){
+                ?>
+                    <ul>
+                        <li>
+                            <img ng-src="http://ebeema.com/ebeema/frontend/web/css/images/logo/nlic.png" alt="NLIC" src="http://ebeema.com/ebeema/frontend/web/css/images/logo/nlic.png">
+                            <p class="ng-binding"><?PHP echo $row->companies_name; ?> - </p>
+                            <p class="ng-binding"><?php echo $row->policies_name; ?></p>
+
+                        </li>
+                        <li class="growth">
+                            <div class="ttl hidden-md hidden-lg">PREMIUM</div>
+                            <h3 class="premium-col ng-binding">
+                                <?php
+                                    echo "Rs. " . round($row->inv_per_year/12, 2) . ' per month <br />';
+                                ?>
+
+                            </h3>
+                        </li>
+                        <li class="growth">
+                            <div class="ttl hidden-md hidden-lg">Premium Paying Years</div>
+                            <h3 class="ng-binding"><i class="fa fa-calendar"></i>
+                            </h3>
+                                <?php
+                                echo $row->term . ' years ' ;
+                                ?>
+                            </h3>
+                        </li>
+                        
+                        <li>
+                            <span>Expected Return</span>
+                            <h3 class="ng-binding">
+                                <?PHP
+                                    echo $row->expected_return;
+                                ?>
+                            </h3>
+                        </li>
+                        
+                        <li>
+                            <a href="<?PHP echo base_url();?>form/orderreceived/<?PHP echo $row->id;?>"><span class="btn btn-sm btn-primary">Book Now <i class="fa fa-arrow-right"></i></span></a>
+                        </li>
+                    </ul>
+                <?php
+                }
+
+                ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
